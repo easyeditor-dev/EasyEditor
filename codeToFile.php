7f73c805f15d8db1db6eed367f36370caa04133c
Taehwan KIm <maxtortime@gmail.com>
2015-12-16 13:21:16 +0900

    다양한 언어와 테마 지원 아직 불러왔을 때 제대로 하이라이팅이 안되는 버그 있음

--- codeToFile.php
+++ codeToFile.php
@@ -20,4 +20,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     fclose($code_file);
 }
 
-?>
--- index.php
+++ index.php
@@ -29,13 +29,14 @@
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+              data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">EasyEditor</a>
+      <a class="navbar-brand" href="/">Easy Editor</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -45,7 +46,7 @@
         <li><a id="load" href="#">불러오기</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">정보</a></li>
+        <li><a href="info.php">정보</a></li>
         <li><a href="#">Dropbox로 로그인</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
@@ -54,15 +55,69 @@
 
 <form id="lang_form">
     <label> 언어를 선택하세요
-        <select name="lang" form="lang_form">
-            <option value="c_cpp">c</option>
-            <option value="c_cpp">cpp</option>
-            <option value="javascript">js</option>
+        <select id="lang" name="lang" form="lang_form">
+            <option value="c_cpp">C</option>
+            <option value="c_cpp">C++</option>
+            <option value="csharp">C#</option>
+            <option value="css">CSS</option>
+            <option value="java">Java</option>
+            <option value="javascript">JavaScript</option>
+            <option value="json">JSON</option>
+            <option value="html">HTML</option>
+            <option value="makefile">MakeFile</option>
+            <option value="markdown">MarkDown</option>
+            <option value="mysql">MySQL</option>
+            <option value="php">PHP</option>
+            <option value="python">Python</option>
+            <option value="mysql">MySQL</option>
+            <option value="plain_text">Plain Text</option>
         </select>
     </label>
     <label>
         파일 이름을 입력하세요
         <input id="filename" type="text">
+    </label>
+    <br>
+    <label> 컬러 테마 변경
+        <select id="theme" name="theme" form="lang_form">
+            <option value="ambiance">ambiance</option>
+            <option value="chaos">chaos</option>
+            <option value="chrome">chrome</option>
+            <option value="clouds">clouds</option>
+            <option value="clouds_midnight">clouds_midnight</option>
+            <option value="cobalt">cobalt</option>
+            <option value="crimson_editor">crimson_editor</option>
+            <option value="dawn">dawn</option>
+            <option value="dreamweaver">dreamweaver</option>
+            <option value="eclipse">eclipse</option>
+            <option value="github">github</option>
+            <option value="idle_fingers">idle_fingers</option>
+            <option value="iplastic">iplastic</option>
+            <option value="katzenmilch">katzenmilch</option>
+            <option value="kr_theme">kr_theme</option>
+            <option value="kuroir">kuroir</option>
+            <option value="merbivore">merbivore</option>
+            <option value="merbivore_soft">merbivore_soft</option>
+            <option value="mono_industrial">mono_industrial</option>
+            <option value="monokai">monokai</option>
+            <option value="pastel_on_dark">pastel_on_dark</option>
+            <option value="solarized_dark">solarized_dark</option>
+            <option value="solarized_light">solarized_light</option>
+            <option value="sqlserver">sqlserver</option>
+            <option value="terminal">terminal</option>
+            <option value="textmate">textmate</option>
+            <option value="tomorrow">tomorrow</option>
+            <option value="tomorrow_night">tomorrow_night</option>
+            <option value="tomorrow_night_blue">tomorrow_night_blue</option>
+            <option value="tomorrow_night_bright">tomorrow_night_bright</option>
+            <option value="tomorrow_night_eighties">tomorrow_night_eighties</option>
+            <option value="twilight">twilight</option>
+            <option value="vibrant_link">vibrant_link</option>
+            <option value="xcode">xcode</option>
+        </select>
+    </label>
+    <label>코드 사이즈 변경 (입력하시면 적용됩니다)
+        <input id="size" type="number" name="fontSize" value="15">
     </label>
 </form>
 
