c8927e1d688e75c53df63779392b588ab5d9ddad
Taehwan KIm <maxtortime@gmail.com>
2015-12-18 03:26:27 +0900

    Dropbox 로그인 기능 추가 및 파일 업로드

--- codeToFile.php
+++ codeToFile.php
@@ -6,17 +6,33 @@
  * Time: 오후 9:52
  */
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+global $dbxClient;
+require_once "dropbox-sdk/Dropbox/autoload.php";
 
+use \Dropbox as dbx;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $filename = $_POST["filename"];
+
+    session_start();
+
+    if($_SESSION["token"]) {
+        $dbxClient = new dbx\Client($_SESSION["token"], "PHP-Example/1.0");
+    }
 
     $code_file = fopen("UserFile/".$filename,"w");
 
     $code = $_POST["code"];
 
+    fwrite($code_file,$code);
+
     echo $filename;
 
-    fwrite($code_file,$code);
+    if($dbxClient) {
+        $result = $dbxClient->uploadFile("/UserFile/".$filename, dbx\WriteMode::add(), $code_file);
+
+    }
     fclose($code_file);
+    //print_r($result);
 }
 
