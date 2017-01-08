3b7b0329a6290dd8b331ee69089221a66b1280c0
Taehwan Kim <maxtortime@gmail.com>
2017-01-09 00:48:05 +0900

    heroku env error fixed

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 DEBUG = True
 BABEL_DEFAULT_LOCALE = 'ko'
-USER_FILE_DIR_PATH = os.path.join('static',  'UserFile')
+USER_FILE_DIR_PATH = os.path.join('static', 'UserFile')
 
 LANGUAGES = {
-    'ko' : 'Korean',
-    'en' : 'English',
-    'ja' : 'Japanese'
+    'ko': 'Korean',
+    'en': 'English',
+    'ja': 'Japanese'
 }
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
@@ -16,7 +16,7 @@ CSRF_ENABLED = True
 CSRF_SESSION_KEY = "5587fa76fb736bd9fe38271a5d0fca38"
 
 # Database
-SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'super-secret'
 
@@ -30,9 +30,10 @@ THREADS_PER_PAGE = 2
 
 SECURITY_REGISTERABLE = True
 SECURITY_CONFIRMABLE = False
+
 # 회원가입 인증 메일 보내기 기능 켜기/끄기
 SECURITY_SEND_REGISTER_EMAIL = False
 SECURITY_PASSWORD_HASH = 'bcrypt'  # 비밀번호 암호화 Hash algorithm  설정
-SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
+SECURITY_PASSWORD_SALT = os.environ["SECURITY_PASSWORD_SALT"]
 SECURITY_RECOVERABLE = True  # 비밀번호 초기화 기능 켜기/끄기
-SECURITY_CHANGEABLE = True # 비밀번호 바꾸기 기능 켜기/끄기
+SECURITY_CHANGEABLE = True  # 비밀번호 바꾸기 기능 켜기/끄기
