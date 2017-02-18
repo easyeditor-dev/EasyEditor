35d428c1dbbc063778c07996f9a2c65ae1237f24
Taehwan Kim <maxtortime@gmail.com>
2017-02-18 16:35:34 +0900

    Add firebase code

--- test_easyeditor.py
+++ test_easyeditor.py
@@ -1,9 +1,19 @@
 import unittest
+from easyeditor import easy_editor
+
+from flask_testing import TestCase
 
 
-class EasyEditorTestCase(unittest.TestCase):
-    def test_static_dir(self):
-        pass
+class EasyEditorTestCase(TestCase):
+    def create_app(self):
+        return easy_editor
+
+    def test_index(self):
+        self.client.get('/')
+        self.assert_template_used('index.html')
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
